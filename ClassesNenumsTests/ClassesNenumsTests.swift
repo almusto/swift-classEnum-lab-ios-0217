729e55467f2a93c0b677e7d023494a96e90ac419
almusto <ales.musto@gmail.com>
2017-02-09 11:36:11 -0500

    enumms

--- ClassesNenumsTests/ClassesNenumsTests.swift
+++ ClassesNenumsTests/ClassesNenumsTests.swift
@@ -19,9 +19,19 @@ class ClassesNenumsTests: XCTestCase {
         super.tearDown()
     }
     
-    func test
-    
-    
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
 
-    
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
+    }
+  }
+
+
+
+
 }
